519481e9a6b71993a4cd0d55a4576dadd0a7a939
Jordan Wright <jmwright798@gmail.com>
2020-12-06 16:11:14 -0600

    Terraform setup (#1)
    * [wip] Working on a PyPI feed that fetches updates from PyPI's RSS feed
    
    * [wip] Initial commit of Terraform deployment workflow
    
    * Fixing indentation
    
    * Moved project info to variables for easier development
    
    * Fixing project name
    
    * Updating local bucket

---
+++ terraform/feeds.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "oss-feeds-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+locals {
+  services = [
+    "cloudbuild.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudscheduler.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "services" {
+  for_each           = toset(local.services)
+  service            = each.value
+  disable_on_destroy = false
+}
+
+resource "google_pubsub_topic" "feed-topic" {
+  name = "feed-topic"
+}
+
+resource "google_storage_bucket" "feed-functions-bucket" {
+  name          = "${var.project}-feed-functions-bucket"
+  force_destroy = true
+}
