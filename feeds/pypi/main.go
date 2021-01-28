442ffb0ad68a71baaf2e98ecae4e9c56e7c95476
Jordan Wright <jmwright798@gmail.com>
2021-01-27 21:58:05 -0600

    Updating code to publish to topic

--- feeds/pypi/main.go
+++ feeds/pypi/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/jordan-wright/ossmalware/pkg/library"
+	"github.com/jordan-wright/ossmalware/pkg/processor"
+
+	"gocloud.dev/pubsub"
+	_ "gocloud.dev/pubsub/gcppubsub"
 )
 
 const (
@@ -19,14 +28,23 @@ type Response struct {
 }
 
 type Package struct {
+	Title        string      `xml:"title"`
 	ModifiedDate rfc1123Time `xml:"pubDate"`
 	Link         string      `xml:"link"`
-	Name         string      `xml:"-"`
-	Version      string      `xml:"-"`
 }
 
 type rfc1123Time struct {
 	time.Time
+}
+
+func (p *Package) Name() string {
+	// The XML Feed has a "Title" element that contains the package and version in it.
+	return strings.Split(p.Title, " ")[0]
+}
+
+func (p *Package) Version() string {
+	// The XML Feed has a "Title" element that contains the package and version in it.
+	return strings.Split(p.Title, " ")[1]
 }
 
 func (t *rfc1123Time) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
@@ -63,20 +81,46 @@ func fetchPackages() ([]*Package, error) {
 // Poll receives a message from Cloud Pub/Sub. Ideally, this will be from a
 // Cloud Scheduler trigger every `delta`.
 func Poll(w http.ResponseWriter, r *http.Request) {
-	log.Print("Executing poll")
+	topicURL := os.Getenv("OSSMALWARE_TOPIC_URL")
+	topic, err := pubsub.OpenTopic(context.TODO(), topicURL)
+	if err != nil {
+		panic(err)
+	}
+
 	packages, err := fetchPackages()
 	if err != nil {
+		log.Printf("error fetching packages: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	cutoff := time.Now().UTC().Add(-delta)
+	processed := 0
 	for _, pkg := range packages {
+		log.Println("Processing:", pkg.Name(), pkg.Version())
 		if pkg.ModifiedDate.Before(cutoff) {
 			continue
 		}
-		// TODO: publish the package up to a cloud pub/sub for processing
-		packages = append(packages, pkg)
+		processed++
+		msg := library.Package{
+			Name:    pkg.Name(),
+			Version: pkg.Version(),
+			Type:    processor.TypePyPI,
+		}
+		b, err := json.Marshal(msg)
+		if err != nil {
+			log.Printf("error marshaling message: %#v", msg)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if err := topic.Send(context.TODO(), &pubsub.Message{
+			Body: b,
+		}); err != nil {
+			log.Printf("error sending package to upstream topic %s: %v", topicURL, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
+	log.Printf("Processed %d packages", processed)
 	w.Write([]byte("OK"))
 }
 
